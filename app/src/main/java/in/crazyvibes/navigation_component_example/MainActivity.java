895f9171a163ef554cefe9d4576ed34e21851dd9
birju <birju.kumar@waycool.in>
2020-07-17 22:55:28 +0530

    Initial commit

---
+++ app/src/main/java/in/crazyvibes/navigation_component_example/MainActivity.java
@@ -0,0 +1,21 @@
+package in.crazyvibes.navigation_component_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.crazyvibes.navigation_component_example.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/in/crazyvibes/navigation_component_example/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package in.crazyvibes.navigation_component_example.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
