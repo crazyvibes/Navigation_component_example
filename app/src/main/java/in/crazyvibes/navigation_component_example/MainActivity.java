2726f7b2d70f926648e39e46bb7632979fa20dd4
birju <birju.kumar@waycool.in>
2020-07-18 09:38:02 +0530

    added navigation destination

--- app/src/main/java/in/crazyvibes/navigation_component_example/MainActivity.java
+++ app/src/main/java/in/crazyvibes/navigation_component_example/MainActivity.java
@@ -1,17 +1,24 @@
 package in.crazyvibes.navigation_component_example;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
 
 import in.crazyvibes.navigation_component_example.ui.main.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SecondFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
